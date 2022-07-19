93c8312ea38c09babe59790734ecaedec5cc7ba7
J. Nick Koston <nick@koston.org>
2022-07-18 19:29:12 -0500

    feat: switch to using bluetooth-sensor-state-data

--- src/govee_ble/parser.py
+++ src/govee_ble/parser.py
@@ -9,12 +9,11 @@ from __future__ import annotations
 
 import logging
 import struct
-from abc import abstractmethod
 
+from bluetooth_sensor_state_data import BluetoothData
 from home_assistant_bluetooth import BluetoothServiceInfo
-from sensor_state_data import SensorLibrary, SensorUpdate
-from sensor_state_data.data import DeviceClass, SensorData
-from sensor_state_data.units import SIGNAL_STRENGTH_DECIBELS_MILLIWATT, TEMP_CELSIUS
+from sensor_state_data import SensorLibrary
+from sensor_state_data.units import TEMP_CELSIUS
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -40,33 +39,6 @@ def decode_temps_probes(packet_value: int) -> float:
     if packet_value < 0:
         return 0.0
     return float(packet_value / 100)
-
-
-class BluetoothData(SensorData):
-    """Update bluetooth data."""
-
-    @abstractmethod
-    def update(self, data: BluetoothServiceInfo) -> None:
-        """Update the data."""
-
-    def supported(self, data: BluetoothServiceInfo) -> bool:
-        """Return True if the device is supported."""
-        self.generate_update(data)
-        return bool(self._device_id_to_type)
-
-    def generate_update(self, data: BluetoothServiceInfo) -> SensorUpdate:
-        """Update a bluetooth device."""
-        self.update_rssi(data.rssi)
-        return super().generate_update(data)
-
-    def update_rssi(self, native_value: int | float) -> None:
-        """Quick update for an rssi sensor."""
-        self.update_sensor(
-            key="rssi",
-            native_unit_of_measurement=SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
-            native_value=native_value,
-            device_class=DeviceClass.SIGNAL_STRENGTH,
-        )
 
 
 class GoveeBluetoothDeviceData(BluetoothData):
